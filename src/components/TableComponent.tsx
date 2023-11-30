621f27f134cd9ed5221a9343ce69b38e6617bbee
Mashentsev Danil <danil.mashentsev@mail.ru>
2023-11-30 17:06:36 +0300

    Axios installed, universities table, some styles in index.css

--- src/components/TableComponent.tsx
+++ src/components/TableComponent.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import { Table, Tag } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Table, Tag, Button } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
+import axios from 'axios';
 
 interface DataType {
-  key: string;
-  name: string;
-  author: string;
-  pages: number;
-  tags: string[];
+    name: string;
+    country: string;
 }
 
 const columns: ColumnsType<DataType> = [
@@ -15,75 +13,35 @@ const columns: ColumnsType<DataType> = [
     title: 'Name',
     dataIndex: 'name',
     key: 'name',
-    render: (text) => <a>{text}</a>,
+    render: (text) => <a >{text}</a>,
   },
   {
-    title: 'Author',
-    dataIndex: 'author',
-    key: 'author',
+    title: 'Country',
+    dataIndex: 'country',
+    key: 'country',
   },
-  {
-    title: 'Pages',
-    dataIndex: 'pages',
-    key: 'pages',
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: (_, { tags }) => (
-      <>
-        {tags.map((tag) => {
-          let color = 'green'
-          if (tag === 'unavailable') {
-            color = 'volcano';
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-    ),
-  },
-];
-
-const data: DataType[] = [
-    {
-        key: '1',
-        name: "1984",
-        author: "George Orwell",
-        pages: 328,
-        tags: ["available"]
-      },
-      {
-        key: '2',
-        name: "To Kill a Mockingbird",
-        author: "Harper Lee",
-        pages: 336,
-        tags: ["available"]
-      },
-      {
-        key: '3',
-        name: "The Great Gatsby",
-        author: "F. Scott Fitzgerald",
-        pages: 180,
-        tags: ["unavailable"]
-      },
-      {
-        key: '4',
-        name: "Harry Potter and the Sorcerer's Stone",
-        author: "J.K. Rowling",
-        pages: 320,
-        tags: ["available"]
-      }
 ];
 
 function TableComponent() {
+    const LIMIT_LIST_SCHOOL = 10;
+    const [page, setPage] = useState<number>(1)
+    const [dataSource, setDataSource] = useState<DataType[]>()
+    const getUniversity = async (page: number, limit: number) => {
+        const response = await axios.get(`http://universities.hipolabs.com/search?offset=${page*limit}&limit=${limit}`)
+        setDataSource(response.data);    
+    }
+    useEffect(() => {
+        getUniversity(page, LIMIT_LIST_SCHOOL)
+    }, [page])
+
     return (
         <>
-            <Table columns={columns} dataSource={data} />;
+            <Table dataSource={dataSource} columns={columns} pagination={false}/>
+            <div className='table__page-buttons'>
+                <Button onClick={() => setPage(page - 1)} disabled={!(page-1)}>Назад</Button>
+                <p className='table__page-count'>{page}</p>
+                <Button onClick={() => setPage(page + 1)}>Вперед</Button>
+            </div>
         </>
     )
 }
