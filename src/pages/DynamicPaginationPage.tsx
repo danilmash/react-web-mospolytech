720f37d5cd844a5ee49eed32d030b894a4bb3043
danilmash <danil.mashentsev@mail.ru>
2024-05-19 03:25:13 +0300

    Не получилось исправить ошибку

--- src/pages/DynamicPaginationPage.tsx
+++ src/pages/DynamicPaginationPage.tsx
@@ -1,16 +1,18 @@
 import {FC, useEffect, useState} from "react";
-import {IUniversity} from "../components/UniversityCard";
+import CardUniversity, {IUniversity} from "../components/UniversityCard";
+import {useInView} from 'react-intersection-observer';
 import axios from "axios";
-import { useInView } from "react-intersection-observer";
-import CardUniversity from "../components/UniversityCard";
 import styled from "styled-components";
 
-const LIMIT_UNIVERSITIES = 15
+
+const LIMIT_UNIVERSITIES = 15;
+
 const ListStyled = styled.div`
     display: flex;
     flex-direction: column;
     gap: 10px;
 `
+
 const BlockObserver = styled.div`
     height: 40px;
     background-color: black;
@@ -21,9 +23,13 @@ const DynamicPagination: FC = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const [loading, setLoading] = useState(false);
 
+    useEffect(() => {
+        fetchUniversities()
+    }, [currentPage])
+
     const {ref, inView} = useInView({
         threshold: 1.0,
-    }); 
+    });
 
     useEffect(() => {
         if (inView) {
@@ -33,22 +39,16 @@ const DynamicPagination: FC = () => {
 
     const fetchUniversities = async () => {
         try {
-            setLoading(true)
-            const offset = (currentPage - 1) * LIMIT_UNIVERSITIES // Укажите свой лимит в константах компонента
-            const {data} = await axios.get(`http://universities.hipolabs.com/search?offset=${offset}&limit=${LIMIT_UNIVERSITIES}`)
-            console.log(data.length)
-            setUniversities((prev) => [...prev, ...data])
+            const offset = (currentPage - 1) * LIMIT_UNIVERSITIES
+            const response = await axios.get(`http://universities.hipolabs.com/search?offset=${offset}&limit=${LIMIT_UNIVERSITIES}`)
+            setUniversities((prev) => [...prev, ...response.data])
+            console.log(response.data)
         } catch (error) {
             console.log('Error fetching univer...', error)
         } finally {
             setLoading(false)
         }
     }
-
-    useEffect(() => {
-        fetchUniversities()
-    }, [currentPage])
-
     return (
         <ListStyled>
             <h1>List Universities</h1>
@@ -57,11 +57,10 @@ const DynamicPagination: FC = () => {
                     <CardUniversity data={university} key={university.name}></CardUniversity>
                 ))
             }
-            {loading && <div>Загрузка...</div>}
+            {loading && <div>Loading...</div>}
             {!loading && <BlockObserver ref={ref}></BlockObserver>}
         </ListStyled>
     )
 }
 
-
-export default DynamicPagination
+export default DynamicPagination;
